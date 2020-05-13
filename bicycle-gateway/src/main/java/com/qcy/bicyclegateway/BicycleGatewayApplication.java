624e10c336a9a25f30c5afc7ac2dbfb25937db88
qianchenyang <qcy949562049>
2020-05-13 09:47:37 +0800

    feat: 项目初始化

---
+++ bicycle-gateway/src/main/java/com/qcy/bicyclegateway/BicycleGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.qcy.bicyclegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicycleGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicycleGatewayApplication.class, args);
+	}
+
+}
---
+++ bicycle-gateway/src/test/java/com/qcy/bicyclegateway/BicycleGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qcy.bicyclegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicycleGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
