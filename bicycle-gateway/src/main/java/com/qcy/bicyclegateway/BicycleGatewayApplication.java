556517d6061a8f4d4175e12aafd69e98e5fe46a6
qianchenyang <qianchenyang@jiuxiniot.com>
2020-08-18 08:49:37 +0800

    feat:项目初始化

--- bicycle-gateway/src/main/java/com/qcy/bicyclegateway/BicycleGatewayApplication.java
+++ bicycle-gateway/src/main/java/com/qcy/bicyclegateway/BicycleGatewayApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BicycleGatewayApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BicycleGatewayApplication.class, args);
 	}
 
